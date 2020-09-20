20c881b11aae27cc617690adb28c8b1ba85d0e89
tai-hona <taihona37@gmail.com>
2020-09-20 19:55:54 +1200

    dom challenge 1 complete

--- classes.js
+++ classes.js
@@ -5,8 +5,10 @@ document.addEventListener('DOMContentLoaded', start)
 
 function start () {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
@@ -19,7 +21,19 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two () {
+  var two = document.getElementById('two')
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
+function three () {
+  var three = document.getElementById('three')
+  three.classList.add('yellow')
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible (){
+  var makeVisible = document.getElementsByClassName('[0]')
+  makeVisible.classList.add('purple')
+}
